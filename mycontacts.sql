4a6bbe52648aae788a514da26e2cd50eaae7ac48
Ascaelle <ascaelleruth@gmail.com>
2023-02-07 19:19:36 +0100

    new database

--- mycontacts.sql
+++ mycontacts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 31, 2023 at 03:03 AM
+-- Generation Time: Feb 07, 2023 at 12:20 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `Id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`Id`, `email`, `password`) VALUES
+(1, 'simon@gmail.com', 'b589d5c1abf5a38922ffd52bffb3321be24e7210'),
+(2, 'ascaelleruth@gmail.com', 'b589d5c1abf5a38922ffd52bffb3321be24e7210'),
+(3, 'no@name', 'e21bfc14ad6d40e861c7ffaeba574bb61e9ae49f'),
+(4, 'noname@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `contacts`
 --
 
@@ -33,38 +55,60 @@ CREATE TABLE `contacts` (
   `email` varchar(30) NOT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `address` varchar(200) DEFAULT NULL,
-  `create_at` datetime NOT NULL DEFAULT current_timestamp()
+  `create_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `admin_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `contacts`
 --
 
-INSERT INTO `contacts` (`id`, `name`, `email`, `phone`, `address`, `create_at`) VALUES
-(3, 'Levie Danielle', 'leviedanielle@gmail.com', '677845244', 'Yaounde biteng', '2023-01-30 15:47:06'),
-(4, 'Harmonie Hargol Biakop', 'harmonie@gmail.com', '665241512', 'Bafoussam Ndiengdam', '2008-05-15 00:00:00'),
-(11, 'Simon Pierre', 'simon@gmail.com', '222301704', 'Galile', '2004-06-06 03:40:00');
+INSERT INTO `contacts` (`id`, `name`, `email`, `phone`, `address`, `create_at`, `admin_id`) VALUES
+(1, 'Ascaelle Ruth ', 'ascaelleruth@gmail.com', '656335346', 'Rue Martin Tabi Essomba', '2023-02-05 23:20:53', 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- Indexes for table `contacts`
 --
 ALTER TABLE `contacts`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `contacts__admin_id__fk` (`admin_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD CONSTRAINT `contacts__admin_id__fk` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
